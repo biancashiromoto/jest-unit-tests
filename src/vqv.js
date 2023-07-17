8145372f653f76e2eb1f186fe1047318116531d2
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-07-17 19:31:39 -0300

    Refactor function vqv

--- src/vqv.js
+++ src/vqv.js
@@ -23,9 +23,7 @@ const vqv = (nome, idade) => {
     return undefined;
   }
   return `Oi, meu nome é ${nome}!\n`
-  + `Tenho ${idade} anos,\n`
-  + 'trabalho na Trybe e mando muito em programação!\n'
-  + '#VQV!';
+  + `Tenho ${idade} anos`;
 };
 
 module.exports = vqv;
